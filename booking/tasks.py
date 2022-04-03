813dc979138dd4682d78e2d6994d8c77af35b1e4
Ezequiel Salas <ezjava@gmail.com>
2022-04-03 19:57:43 -0400

    Adding a comment

--- booking/tasks.py
+++ booking/tasks.py
@@ -1,8 +1,5 @@
-from celery import shared_task
-from celery import Celery
 from celery_once import QueueOnce
 from celery.utils.log import get_task_logger
-from rest_framework.exceptions import ValidationError
 
 from booking.models import Booking
 from booking_core.celery import app
@@ -17,7 +14,7 @@ def reserve_room(room_id):
 
     if not Booking.book(room_id):
         logger.info(f"Room {room_id} is not available")
-        # TODO: Notify the user because the asynchronous behavior
+        # TODO: Notify via websocket to the user because of the asynchronous behavior
         return
 
     logger.info(f"Room {room_id} booked successfully")
